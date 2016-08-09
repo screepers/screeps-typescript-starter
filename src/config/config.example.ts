40803801ccd94ffa18819b4daca7af98cc506d60
Resi Respati <resir014@gmail.com>
2016-08-10 01:23:48 +0700

    Added USE_PATHFINDER constant in config.ts

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -10,9 +10,12 @@ export const VERBOSE: boolean = true;
 export const MAX_HARVESTERS_PER_SOURCE: number = 4;
 
 /**
- * Default amount of minimal ticksToLive Screep can have, before it goes to renew.
- * This is only default value, that don't have to be used.
- * So it doesn't cover all Screeps.
+ * Enable this to use the experimental PathFinder class.
+ */
+export const USE_PATHFINDER: boolean = true;
+
+/**
+ * Minimum number of ticksToLive before a Creep before they go to renew.
  * @type {number}
  */
 export const DEFAULT_MIN_LIFE_BEFORE_NEEDS_REFILL: number = 700;
--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,11 @@ RoomManager.loadRooms();
 SpawnManager.loadSpawns();
 SourceManager.loadSources();
 
-// Screeps" system expects this "loop" method in main.js to run the application.
+if (Config.USE_PATHFINDER) {
+  PathFinder.use(true);
+}
+
+// Screeps system expects this "loop" method in main.js to run the application.
 // If we have this line, we can make sure that globals bootstrap and game loop work.
 // http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
 
