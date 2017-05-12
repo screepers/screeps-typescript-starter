547742dd95f13de33ada77e5dd2c248791a2b045
Bryan Becker <mrbryanbecker@gmail.com>
2017-05-12 13:07:26 +0800

    Add source map loader config for ts/tsx files (as per docs recommendation)

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ module.exports = {
     rules: [
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: 'source-map-loader', enforce: 'pre' },
+      { test: /\.tsx?$/, loader: 'source-map-loader', enforce: 'pre' },
       ////
       // typescript rules
       {
