b8ad69811f3c22ddb4265ac35e4c79aa4f3e5b4b
Bryan Becker <mrbryanbecker@gmail.com>
2017-05-13 08:47:39 +0800

    Git revision info added to script; Log find/replace partially impl.
    The find/replace of log info is currently untested.
    I was unable to add a replacement for `@@_repo_@@`, as the webpack
    git revisions plugin does not provide that information

--- config/config.defaults.js
+++ config/config.defaults.js
@@ -1,6 +1,8 @@
 const { CheckerPlugin, TsConfigPathsPlugin } = require('awesome-typescript-loader');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const { Config } = require('webpack-config');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
 
 
@@ -62,7 +64,18 @@ module.exports = new Config().merge({
     new CleanWebpackPlugin(
       [ 'dist/[env]/*.*' ],  // array of paths to clean
       { root: '[root]' }
-    )
+    ),
+    // This plugin provides acces to git info throughout this script
+    new GitRevisionPlugin({ branch: true }),
+    // you can use this to define build toggles
+    //  keys defined here will be replaced in the
+    //  output code with their values
+    // Note that because the plugin does a direct text replacement, the value given to it must include actual quotes inside of the string itself. Typically, this is done either with either alternate quotes, such as '"production"', or by using JSON.stringify('production'). 
+    new webpack.DefinePlugin({
+      '@@_repo_@@': JSON.stringify('UNEMPLEMENTED'),
+      '@@_revision_@@': JSON.stringify('[git-revision-version]'),
+      PRODUCTION: JSON.stringify(true)
+    })
   ],
 
   module: {
