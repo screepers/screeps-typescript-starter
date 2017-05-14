ef40837ba2cc362dc24fcf058485e3c9af0689e3
Bryan Becker <mrbryanbecker@gmail.com>
2017-05-14 13:15:56 +0800

    Remove accidentally commited experimental code

--- typings/vendor.d.ts
+++ typings/vendor.d.ts
@@ -1,19 +1,12 @@
 /* tslint:disable */
 // Put shims and extensions to installed modules and typings here
 
-
 // add objects to `global` here
 declare namespace NodeJS {
   interface Global {
     log: any;
   }
 }
-
-// type CreepMemory = any;
-interface CreepMemory {}
-interface FlagMemory {}
-interface RoomMemory {}
-interface SpawnMemory {}
 
 // shim uglify-js for webpack
 declare module "uglify-js" {
