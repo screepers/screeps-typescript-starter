fc0afff133833f8faa72d032321a6605ab8a9ffc
Bryan Becker <mrbryanbecker@gmail.com>
2017-05-14 12:31:44 +0800

    Track typings; Add vendor typings file

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/screeps/index.d.ts" />
+/// <reference path="modules/tapable/index.d.ts" />
+/// <reference path="modules/webpack-chain/index.d.ts" />
+/// <reference path="modules/webpack/index.d.ts" />
