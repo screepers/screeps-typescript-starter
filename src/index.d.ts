0a695299f9539e2f5224dc630b45bd7e552288e3
Bryan Becker <mrbryanbecker@gmail.com>
2017-06-04 15:12:20 +0800

    Install profiler and add comments

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,17 @@
 interface Memory {
   uuid: number;
   log: any;
+  P: Profiler;
+}
+
+// add objects to `global` here
+// NodeJS already declares global, so we need to extend it here:
+// tslint:disable-next-line
+declare namespace NodeJS {
+  interface Global {
+    log: any;
+    P: Profiler;
+  }
 }
 
 declare const __REVISION__: string;
