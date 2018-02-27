12b63845bcedd6e9f542bdbf3fdc33e1bb890e0a
Resi Respati <resir014@gmail.com>
2018-02-27 14:59:35 +0700

    clarify what types.d.ts is for

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,5 @@
 // type shim for nodejs' `require()` syntax
+// for stricter node.js typings, remove this and install `@types/node`
 declare const require: (module: string) => any;
+
+// add your custom typings here
