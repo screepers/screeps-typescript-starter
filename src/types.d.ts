0a683c15e27e05051bd4adf97bf0d1b927dc8403
Resi Respati <resir014@gmail.com>
2018-08-01 12:35:46 +0700

    improve declaration examples

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,20 @@
-// add your custom typings here
+// example declaration file - remove these and add your own custom typings
+
+// memory extension samples
+interface CreepMemory {
+  role: string;
+  room: string;
+  working: boolean;
+}
+
+interface Memory {
+  uuid: number;
+  log: any;
+}
+
+// `global` extension samples
+declare namespace NodeJS {
+  interface Global {
+    log: any;
+  }
+}
