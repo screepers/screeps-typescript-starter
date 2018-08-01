0b58638297263993191474c423e02b7b7bfacbe8
Resi Respati <resir014@gmail.com>
2018-08-01 12:31:53 +0700

    latest rollup requires @types/node

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,1 @@
-// type shim for nodejs' `require()` syntax
-// for stricter node.js typings, remove this and install `@types/node`
-declare const require: (module: string) => any;
-
 // add your custom typings here
