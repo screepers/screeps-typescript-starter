a3517e7b8ea9dbf05e6df39678f04c8af0961ab6
UhmBrock <uhmbrock@hotmail.com>
2019-01-02 00:55:42 -0600

    Changed types.d.ts to use proper comments
    This enables inline documentation

--- src/Api/Spawn.Api.ts
+++ src/Api/Spawn.Api.ts
@@ -1,14 +1,20 @@
 import { RoomHelper } from "../Helpers/RoomHelper"
 
-// the api used by the spawn manager
+/**
+* the api used by the spawn manager
+*/
 export class SpawnApi {
 
-    // get count for the specified creep
+    /**
+    * get count for the specified creep
+    */
     public static getCreepCount(room: Room, creepConst: RoleConstant): number {
         return RoomHelper.getNumCreepsInRoomBy(room, creepConst);
     }
 
-    // get the limit for the specified creep
+    /**
+    * get the limit for the specified creep
+    */
     public static getCreepLimits(room: Room, creepConst: RoleConstant): CreepLimits {
         return room.memory.creepLimit[creepConst];
     }
