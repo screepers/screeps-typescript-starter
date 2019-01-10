75bd6ad95c112b6bb4c52a6b36f67dcef78db538
UhmBrock <uhmbrock@hotmail.com>
2019-01-10 12:58:06 -0600

    UtilHelper

---
+++ src/Helpers/UtilHelper.ts
@@ -0,0 +1,24 @@
+import { ERROR_FATAL, ERROR_ERROR, ERROR_WARN, COLORS } from "utils/Constants";
+import { ErrorConstant } from "types";
+
+export default class UtilHelper {
+    /**
+     * Display an error to the console
+     * @param title The title line to display above the error message
+     * @param body The error message to be displayed
+     * @param severity An ErrorConstant (Constants.ts) denoting the severity of the error 
+     * @param useTitleColor [Optional] A hex color value to override the title line's color
+     * @param useBodyColor [Optional] A hex color value to override the body's color
+     */
+    public static throwError(title: string, body: string, severity: ErrorConstant, useTitleColor?: string, useBodyColor?: string): void {
+        
+        // For now we trust that we are passing a proper hex color
+        const titleColor: string = useTitleColor !== undefined ? useTitleColor : COLORS[severity];
+        const bodyColor: string = useBodyColor !== undefined ? useBodyColor : "#da1414";
+        
+        console.log('<font color="' + titleColor + '">' + title + '</font>');
+        console.log('<font color="' + bodyColor + '">' + body + '</font>');
+
+        //
+    }
+}
