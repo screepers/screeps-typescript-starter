a9587c44a1e56c4a0da949f1087a8876c7a52aea
UhmBrock <uhmbrock@hotmail.com>
2019-01-10 03:12:08 -0600

    Update ErrorMapper.ts - Missed default

--- src/utils/ErrorMapper.ts
+++ src/utils/ErrorMapper.ts
@@ -1,7 +1,7 @@
 // tslint:disable:no-conditional-assignment
 import { SourceMapConsumer } from "source-map";
 
-export class ErrorMapper {
+export default class ErrorMapper {
   // Cache consumer
   private static _consumer?: SourceMapConsumer;
 
