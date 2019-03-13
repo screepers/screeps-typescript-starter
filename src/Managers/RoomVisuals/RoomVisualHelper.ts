a234b8fcd1d09d12b71e3a03633acac2fe203d45
Jacob Waldrip <jwaldrip@okcthunder.com>
2019-03-13 08:59:07 -0500

    added idea for helper

--- src/Managers/RoomVisuals/RoomVisualHelper.ts
+++ src/Managers/RoomVisuals/RoomVisualHelper.ts
@@ -1,5 +1,8 @@
 // Helper for room visuals
 export class RoomVisualManager {
 
-
+    // Need to add something for displaying text
+    // Maybe split up into a grid layout idk
+    // display certain sections of memory
+    //this is more for getting objects to give to the api
 }
