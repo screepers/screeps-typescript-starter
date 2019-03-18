de33cb37bb1d795c47d00be8f130aa35be238e24
Jacob Waldrip <jwaldrip@okcthunder.com>
2019-03-18 14:23:30 -0500

    small changes i think

--- src/Managers/Roles/RemoteHarvesterCreepManager.ts
+++ src/Managers/Roles/RemoteHarvesterCreepManager.ts
@@ -15,6 +15,119 @@ export default class RemoteHarvesterCreepManager {
      * @param creep the creep we are running
      */
     public static runCreepRole(creep: Creep): void {
+        if (creep.spawning) {
+            return; // don't do anything until spawned
+        }
 
+        const targetRoom: Room = Game.rooms[creep.memory.targetRoom];
+
+        if (creep.memory.job === undefined) {
+            creep.memory.job = this.getNewJob(creep, targetRoom);
+
+            if (creep.memory.job === undefined) {
+                return; // idle for a tick
+            }
+
+            this.handleNewJob(creep);
+        }
+
+        if (creep.memory.working === true) {
+            CreepApi.doWork(creep, creep.memory.job);
+            return;
+        }
+
+        CreepApi.travelTo(creep, creep.memory.job);
+    }
+
+    /**
+     * Decides which kind of job to get and calls the appropriate function
+     */
+    public static getNewJob(creep: Creep, room: Room): BaseJob | undefined {
+        // if creep is empty, get a GetEnergyJob
+        if (creep.carry.energy === 0) {
+            return this.newGetEnergyJob(creep, room);
+        } else {
+            // Creep energy > 0
+            return this.newCarryPartJob(creep, room);
+        }
+    }
+
+    /**
+     * Get a GetEnergyJob for the harvester
+     */
+    public static newGetEnergyJob(creep: Creep, room: Room): GetEnergyJob | undefined {
+        // All container jobs with enough energy to fill creep.carry, and not taken
+        const containerJobs = MemoryApi.getContainerJobs(
+            room,
+            (cJob: GetEnergyJob) => !cJob.isTaken && cJob.resources!.energy >= creep.carryCapacity
+        );
+
+        if (containerJobs.length > 0) {
+            return containerJobs[0];
+        }
+
+        // All dropped resources with enough energy to fill creep.carry, and not taken
+        const dropJobs = MemoryApi.getPickupJobs(
+            room,
+            (dJob: GetEnergyJob) => !dJob.isTaken && dJob.resources!.energy >= creep.carryCapacity
+        );
+
+        if (dropJobs.length > 0) {
+            return dropJobs[0];
+        }
+
+        // All backupStructures with enough energy to fill creep.carry, and not taken
+        const backupStructures = MemoryApi.getBackupStructuresJobs(
+            room,
+            (job: GetEnergyJob) => !job.isTaken && job.resources!.energy >= creep.carryCapacity
+        );
+
+        if (backupStructures.length > 0) {
+            return backupStructures[0];
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Get a CarryPartJob for the harvester
+     */
+    public static newCarryPartJob(creep: Creep, room: Room): CarryPartJob | undefined {
+        const fillJobs = MemoryApi.getFillJobs(room, (fJob: CarryPartJob) => !fJob.isTaken);
+
+        if (fillJobs.length > 0) {
+            return fillJobs[0];
+        }
+
+        const storeJobs = MemoryApi.getStoreJobs(room, (bsJob: CarryPartJob) => !bsJob.isTaken);
+
+        if (storeJobs.length > 0) {
+            const storageJob: CarryPartJob | undefined = _.find(storeJobs, (storeJob: CarryPartJob) =>
+                !storeJob.isTaken && storeJob.targetType === STRUCTURE_STORAGE
+            );
+
+            if (storageJob) {
+                return storageJob;
+            }
+            else {
+                return storeJobs[0];
+            }
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Handles setup for a new job
+     */
+    public static handleNewJob(creep: Creep): void {
+        if (creep.memory.job!.jobType === "getEnergyJob") {
+            // TODO Decrement the energy available in room.memory.job.xxx.yyy by creep.carryCapacity
+            return;
+        }
+        else if (creep.memory.job!.jobType === "carryPartJob") {
+            // TODO Mark the job we chose as taken
+            return;
+        }
     }
 }
--- src/Managers/Roles/RemoteMinerCreepManager.ts
+++ src/Managers/Roles/RemoteMinerCreepManager.ts
@@ -2,19 +2,86 @@ import RoomApi from "../../Api/Room.Api";
 import MemoryApi from "../../Api/Memory.Api";
 import CreepDomesticApi from "Api/CreepDomestic.Api";
 import CreepApi from "Api/Creep.Api";
+import CreepHelper from "Helpers/CreepHelper";
 import CreepDomestic from "Api/CreepDomestic.Api";
 import {
-    ERROR_WARN
+    ERROR_WARN,
+    ROLE_REMOTE_MINER
 } from "utils/constants";
 
 // Manager for the miner creep role
 export default class RemoteMinerCreepManager {
 
     /**
-     * run the remote miner creep
-     * @param creep the creep we are running
+     * Run the remote miner creep
+     * @param creep The creep to run
      */
     public static runCreepRole(creep: Creep): void {
 
+        if (creep.spawning) {
+            return; // Don't do anything until you've spawned
+        }
+
+        const targetRoom: Room = Game.rooms[creep.memory.targetRoom];
+
+        if (creep.memory.job === undefined) {
+            creep.memory.job = this.getNewSourceJob(creep, targetRoom);
+
+            if (creep.memory.job === undefined) {
+                return; // idle for a tick
+            }
+
+            // Set supplementary.moveTarget to container if one exists and isn't already taken
+            this.handleNewJob(creep);
+        }
+
+        if (creep.memory.working === true) {
+            CreepApi.doWork(creep, creep.memory.job);
+            return;
+        }
+
+        CreepApi.travelTo(creep, creep.memory.job);
+    }
+
+    /**
+     * Find a job for the creep
+     */
+    public static getNewSourceJob(creep: Creep, room: Room): GetEnergyJob | undefined {
+        // TODO change this to check creep options to filter jobs -- e.g. If creep.options.harvestSources = true then we can get jobs where actionType = "harvest" and targetType = "source"
+        const sourceJobs = MemoryApi.getSourceJobs(room, (sjob: GetEnergyJob) => !sjob.isTaken);
+
+        if (sourceJobs.length > 0) {
+            return sourceJobs[0];
+        } else {
+            return undefined;
+        }
+    }
+
+    /**
+     * Handle initalizing a new job
+     */
+    public static handleNewJob(creep: Creep): void {
+        const miningContainer = CreepHelper.getMiningContainer(
+            creep.memory.job as GetEnergyJob,
+            Game.rooms[creep.memory.targetRoom]
+        );
+
+        if (miningContainer === undefined) {
+            return; // We don't need to do anything else if the container doesn't exist
+        }
+
+        const creepsOnContainer = miningContainer.pos.lookFor(LOOK_CREEPS);
+
+        if (creepsOnContainer.length > 0) {
+            if (creepsOnContainer[0].memory.role === ROLE_REMOTE_MINER) {
+                return; // If there is already a miner creep on the container, then we don't target it
+            }
+        }
+
+        if (creep.memory.supplementary === undefined) {
+            creep.memory.supplementary = {};
+        }
+
+        creep.memory.supplementary.moveTargetID = miningContainer.id;
     }
 }
