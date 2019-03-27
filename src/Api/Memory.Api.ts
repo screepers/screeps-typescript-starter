46832fab9a1d3bcb8855af6d08ced3ad9cdd0e8b
UhmBrock <uhmbrock@hotmail.com>
2019-03-27 00:10:14 -0500

    Finished search functions

--- src/Api/Memory.Api.ts
+++ src/Api/Memory.Api.ts
@@ -1230,11 +1230,11 @@ export default class MemoryApi {
             );
         }
 
-        let creepJob: BaseJob = creep.memory.job!;
+        const creepJob: BaseJob = creep.memory.job!;
         let roomJob: BaseJob | undefined;
 
         // Assign room job to the room in memory
-        switch (creep.memory.job!.jobType) {
+        switch (creepJob!.jobType) {
             case "carryPartJob":
                 roomJob = this.searchCarryPartJobs(creepJob as CarryPartJob, room);
                 break;
@@ -1254,6 +1254,12 @@ export default class MemoryApi {
                     ERROR_ERROR
                 );
         }
+
+        if(roomJob === undefined) {
+            throw new UserException("Error in updateJobMemory", "Could not find the job in room memory to update.", ERROR_ERROR);
+        }
+
+        
     }
 
     /**
@@ -1376,6 +1382,26 @@ export default class MemoryApi {
             roomJob = _.find(jobListing.containerJobs.data, (cJob: GetEnergyJob) => cJob.targetID === job.targetID);
         }
 
-        if(roomJob === undefined && jobListing.)
+        if(roomJob === undefined && jobListing.sourceJobs) {
+            roomJob = _.find(jobListing.containerJobs!.data, (sJob: GetEnergyJob) => sJob.targetID === job.targetID);
+        }
+
+        if(roomJob === undefined && jobListing.pickupJobs) {
+            roomJob = _.find(jobListing.pickupJobs!.data, (pJob: GetEnergyJob) => pJob.targetID === job.targetID);
+        }
+
+        if(roomJob === undefined && jobListing.backupStructures) {
+            roomJob = _.find(jobListing.backupStructures!.data, (sJob: GetEnergyJob) => sJob.targetID === job.targetID);
+        }
+
+        if(roomJob === undefined && jobListing.linkJobs) {
+            roomJob = _.find(jobListing.linkJobs!.data, (lJob: GetEnergyJob) => lJob.targetID === job.targetID);
+        }
+
+        if(roomJob === undefined && jobListing.tombstoneJobs) {
+            roomJob = _.find(jobListing.tombstoneJobs!.data, (tJob: GetEnergyJob) => tJob.targetID === job.targetID);
+        }
+
+        return roomJob;
     }
 }
