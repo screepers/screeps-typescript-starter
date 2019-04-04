b411379f82754e7f6a788aa1ee7005d89a5b1718
jakesboy2 <jcwaldrip@att.net>
2019-04-04 00:33:41 -0500

    fixed sim being disabled

--- src/main.ts
+++ src/main.ts
@@ -144,32 +144,32 @@ export const loop = ErrorMapper.wrapLoop(() => {
   }
 
   // run the empire
-  if (Game.cpu['bucket'] > EMPIRE_MANAGER_BUCKET_LIMIT) {
+  if (!Game.cpu['bucket'] || Game.cpu['bucket'] > EMPIRE_MANAGER_BUCKET_LIMIT) {
     try { EmpireManager.runEmpireManager(); } catch (e) { UtilHelper.printError(e); }
   }
 
   // run rooms
-  if (Game.cpu['bucket'] > ROOM_MANAGER_BUCKET_LIMIT) {
+  if (!Game.cpu['bucket'] || Game.cpu['bucket'] > ROOM_MANAGER_BUCKET_LIMIT) {
     try { RoomManager.runRoomManager(); } catch (e) { UtilHelper.printError(e); }
   }
 
   // run spawning
-  if (Game.cpu['bucket'] > SPAWN_MANAGER_BUCKET_LIMIT) {
+  if (!Game.cpu['bucket'] || Game.cpu['bucket'] > SPAWN_MANAGER_BUCKET_LIMIT) {
     try { SpawnManager.runSpawnManager(); } catch (e) { UtilHelper.printError(e); }
   }
 
   // run creeps
-  if (Game.cpu['bucket'] > CREEP_MANAGER_BUCKET_LIMIT) {
+  if (!Game.cpu['bucket'] || Game.cpu['bucket'] > CREEP_MANAGER_BUCKET_LIMIT) {
     try { CreepManager.runCreepManager(); } catch (e) { UtilHelper.printError(e); }
   }
 
   // clean up memory
-  if (Game.cpu['bucket'] > MEMORY_MANAGER_BUCKET_LIMIT) {
+  if (!Game.cpu['bucket'] || Game.cpu['bucket'] > MEMORY_MANAGER_BUCKET_LIMIT) {
     try { MemoryManager.runMemoryManager(); } catch (e) { UtilHelper.printError(e); }
   }
 
   // Display room visuals if we have a fat enough bucket and config option allows it
-  if (Game.cpu['bucket'] > 2000 && ROOM_OVERLAY_ON) {
+  if (!Game.cpu['bucket'] || Game.cpu['bucket'] > 2000 && ROOM_OVERLAY_ON) {
     try { RoomVisualManager.runRoomVisualManager(); } catch (e) { UtilHelper.printError(e); }
   }
   // -------- end managers --------
