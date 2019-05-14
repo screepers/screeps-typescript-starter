e31e187c393334d3be6b681466c323f49eb85769
Jake <jcwaldrip@att.net>
2019-05-14 01:57:26 -0500

    added config

--- src/utils/militaryConfig.ts
+++ src/utils/militaryConfig.ts
@@ -8,17 +8,17 @@ import {
 /**
  * set a zealot flag to one time use
  */
-export const ZEALOT_FLAG_ONE_TIME_USE = true;
+export const ZEALOT_FLAG_ONE_TIME_USE: boolean = true;
 
 /**
  * set a stalker flag to one time use
  */
-export const STALKER_FLAG_ONE_TIME_USE = true;
+export const STALKER_FLAG_ONE_TIME_USE: boolean = true;
 
 /**
  * set a standard squad flag to one time use
  */
-export const STANDARD_SQUAD_FLAG_ONE_TIME_USE = true;
+export const STANDARD_SQUAD_FLAG_ONE_TIME_USE: boolean = true;
 
 /* Arrays for military flags */
 /**
