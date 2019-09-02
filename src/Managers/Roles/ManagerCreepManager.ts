e4f4ae2fa06aed0cd5a247a37fd892ab43628ff2
jakesboy2 <jcwaldrip@att.net>
2019-09-02 16:55:50 -0500

    got manaager manager creep ready to implement job functions

--- src/Managers/Roles/ManagerCreepManager.ts
+++ src/Managers/Roles/ManagerCreepManager.ts
@@ -1,1 +1,71 @@
-// keep
+import MemoryApi from "../../Api/Memory.Api";
+import CreepApi from "Api/Creep.Api";
+import {
+    ROLE_MANAGER,
+} from "utils/constants";
+
+// Manager for the miner creep role
+export default class MinerCreepManager implements ICreepRoleManager {
+
+    public name: RoleConstant = ROLE_MANAGER;
+
+    constructor() {
+        const self = this;
+        self.runCreepRole = self.runCreepRole.bind(this);
+    }
+
+    /**
+     * Run the miner creep
+     * @param creep The creep to run
+     */
+    public runCreepRole(creep: Creep): void {
+
+        const homeRoom: Room = Game.rooms[creep.memory.homeRoom];
+
+        if (creep.memory.job === undefined) {
+            creep.memory.job = this.getNewJob(creep, homeRoom);
+
+            if (creep.memory.job === undefined) {
+                return; // idle for a tick
+            }
+
+            // Set supplementary.moveTarget to container if one exists and isn't already taken
+            this.handleNewJob(creep, homeRoom);
+        }
+
+        if (creep.memory.job) {
+            if (creep.memory.working) {
+                CreepApi.doWork(creep, creep.memory.job);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Decides which kind of job to get and calls the appropriate function
+     * @param creep the creep we are getting the job for
+     * @param room the room we are in
+     * @returns BaseJob of the new job we recieved (undefined if none)
+     */
+    public getNewJob(creep: Creep, room: Room): BaseJob | undefined {
+        // get a new job
+        // TODO
+        return {
+            jobType: "workPartJob",
+            targetID: "3939301",
+            targetType: "constructedWall",
+            actionType: "repair",
+            isTaken: false
+        };
+    }
+
+    /**
+     * Handle initalizing a new job
+     * @param creep the creep we are using
+     * @param room the room we are in
+     */
+    public handleNewJob(creep: Creep, room: Room): void {
+        // Handle new job here
+        // TODO
+    }
+}
