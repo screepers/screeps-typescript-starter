57990c4215b79cc95bdf17d34ba57648b375013b
tcdejong <thijs.casper.de.jong@gmail.com>
2020-02-03 18:41:29 +0100

    Migrate from TSLint to ESLint

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ErrorMapper } from "utils/ErrorMapper";
+import { ErrorMapper } from "./utils/ErrorMapper";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
