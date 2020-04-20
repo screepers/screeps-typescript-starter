7907d3c819d1eb70ac6f2a35db42b79d1ef22fd3
Skyler Kehren <pyrodogg@gmail.com>
2020-04-20 19:38:02 +0300

    Update Node,TypeScript,Rollup dependencies
    @types/lodash@3.10.3 broke the template since lodash is assumed to be
    global. Adds a shim to types file to allow continued use of a global
    lodash since the game provides 3.10 automatically.
    Update rollup to 2.0, and associated rollup plugins

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,12 @@
 // example declaration file - remove these and add your own custom typings
 
+// Shim to allow continued use of global lodash without having to explicitley import in every file.
+// as of @types/lodash@3.10.3
+import _ from "lodash";
+declare global {
+  const _: typeof _;
+}
+
 // memory extension samples
 interface CreepMemory {
   role: string;
