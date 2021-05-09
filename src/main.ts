675329997a9c14a264f9ba98d7eab5435e5605c7
Skyler Kehren <pyrodogg@gmail.com>
2021-05-09 22:38:37 +0300

    Remove types.d.ts
    Moves example type definitions to main.ts from types.d.ts
    Adds additional documentation to example types
    Removes types.d.ts file (fixes #147)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
 import { ErrorMapper } from "utils/ErrorMapper";
 
+declare global {
+  /*
+    Example types, expand on these or remove them and add your own.
+    Note: Values, properties defined here do no fully *exist* by this type definiton alone.
+          You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
+
+    Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
+    Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
+  */
+  // Memory extension samples
+  interface Memory {
+    uuid: number;
+    log: any;
+  }
+
+  interface CreepMemory {
+    role: string;
+    room: string;
+    working: boolean;
+  }
+
+  // Syntax for adding proprties to `global` (ex "global.log")
+  namespace NodeJS {
+    interface Global {
+      log: any;
+    }
+  }
+}
+
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
